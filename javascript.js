9e95d5977dd8a37ae629f7ccd48bf171935a4739
Faiz Mirza <141449876+FaizMirza321@users.noreply.github.com>
2024-10-11 00:40:15 +0100

    square grid with mouse hover event listener added

--- javascript.js
+++ javascript.js
@@ -1,10 +1,22 @@
 const container = document.getElementById("container");
+containerWidth=container.offsetWidth;
 
-function createGrid(size){
-    for(let row=0; row<size**2; row++){
-        let cell = document.createElement("div");
-        cell.style.width = "100% * " + (1/size) + "- 10 - 1";
-        container.appendChild(cell);
+function createGrid(num){   
+    for(let i=0;i<num;i++){
+        const col=document.createElement("col");
+        for(let j=0;j<num;j++){
+            const square=document.createElement("div");
+            square.classList.add("square");
+            square.style.width=(containerWidth/num)+"px";
+            square.style.borderStyle="solid";
+            square.style.height=(containerWidth/num)+"px";
+            square.style.backgroundColor="white";
+            square.addEventListener('mouseenter',()=>{
+                square.style.backgroundColor="green";
+            })
+            col.appendChild(square); 
+        }
+        container.appendChild(col);   
     }
 }
 createGrid(16);
