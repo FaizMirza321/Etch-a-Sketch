d46618ccf2ce3402e5c9d2df7ff273128e30b338
Faiz Mirza <141449876+FaizMirza321@users.noreply.github.com>
2024-10-11 00:19:55 +0100

    width modification

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ const container = document.getElementById("container");
 function createGrid(size){
     for(let row=0; row<size**2; row++){
         let cell = document.createElement("div");
+        cell.style.width = "100% * " + (1/size) + "- 10 - 1";
         container.appendChild(cell);
     }
 }
